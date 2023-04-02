8723b848c87369710add5029b8be6f8db4605740
rsbappy <bappykha226@gmail.com>
2023-04-02 21:20:48 +0600

    todo-app for vue

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+<h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur est voluptatum perferendis? Expedita voluptates magnam fuga nostrum architecto, laboriosam adipisci ad facere odio veniam harum, ullam asperiores, earum consequatur obcaecati?</h1>
+
+</template>
+
+<style scoped>
+
+</style>
