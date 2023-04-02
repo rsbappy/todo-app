7382ae57a3e1c7d39dbdf6466369d0242c0609a0
rsbappy <bappykha226@gmail.com>
2023-04-02 22:12:16 +0600

    task list desgin

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,4 @@
+
 <script setup>
 
 </script>
@@ -6,8 +7,41 @@
 
   <div class="container">
 
+    <div class="row my-5">
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-body">
+            <h4 class="card-title text-center"> Task List</h4>
+            <div class="row mb-3">
+              <form action="">
+                <div class="col-md-12">
+                  <label for="" class="form-label"> Add New Task</label>
+                  <input type="text" class="form-control">
+                </div>
+                <div class="mt-2">
+                  <input type="button" value="Add task" class="btn btn-success">
+                </div>
+              </form>
+            </div>
+          </div>
 
-<h1 class="text-bg-success">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur est voluptatum perferendis? Expedita voluptates magnam fuga nostrum architecto, laboriosam adipisci ad facere odio veniam harum, ullam asperiores, earum consequatur obcaecati?</h1>
+          <div class="card-body">
+            <h4 class="text-danger"> Tasks</h4>
+            <ul class="list-group">
+              <li class="list-group-item d-flex justify-content-between align-items-center">
+                list Item
+                <a href="" class="btn">
+                <i class="fa solid fa-xmark"></i>
+              </a>
+              </li>
+
+          
+
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
 </div>
 </template>
 
