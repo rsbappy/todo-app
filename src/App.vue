c5b486dcc4dae8e078ad1f927c4e4f21f4929700
rsbappy <bappykha226@gmail.com>
2023-04-02 21:44:44 +0600

    bootstrap setup and configar

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,12 @@
 </script>
 
 <template>
-<h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur est voluptatum perferendis? Expedita voluptates magnam fuga nostrum architecto, laboriosam adipisci ad facere odio veniam harum, ullam asperiores, earum consequatur obcaecati?</h1>
 
+  <div class="container">
+
+
+<h1 class="text-bg-success">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur est voluptatum perferendis? Expedita voluptates magnam fuga nostrum architecto, laboriosam adipisci ad facere odio veniam harum, ullam asperiores, earum consequatur obcaecati?</h1>
+</div>
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+ // custom csss link
 
+ import './assets/sass/style.scss'
 const app = createApp(App)
 app.use(createPinia())
 app.mount('#app')
+
+// import all of bootstrap js
+
+import * as bootstrap from 'bootstrap';
